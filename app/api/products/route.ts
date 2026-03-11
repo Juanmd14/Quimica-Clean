3be1088c7be2c7e54f80450c00c9e533373769f3
juanmd14 <juanmd149@gmail.com>
2026-03-11 16:20:42 -0300

    fix4

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,7 @@
 // Products API Route
 
-import { createClient, supabaseAdmin } from '@/lib/supabase'
+import { createClient} from '@/lib/supabase'
+import { supabaseAdmin } from '@/lib/supabaseAdmin'
 const supabase = createClient()
 import { productSchema } from '@/lib/validations'
 import { requireAdminAuth } from '@/lib/adminMiddleware'
